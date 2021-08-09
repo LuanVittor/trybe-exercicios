7add36cbcb74da612bdef0c9d7d73061f238ed8e
Luan Vitor <luan_vittor@hotmail.com>
2021-08-09 16:36:06 -0300

    ex aula feito

---
+++ Fundamentos/bloco-5-dom-eventos-e-web-storage/dia-5-1/ex aula/script.js
@@ -0,0 +1,31 @@
+let verde = document.getElementById("header-container")
+
+verde =document.getElementById("header-container").style.backgroundColor = "rgb(0, 176, 105)";
+
+let laranja = document.getElementsByClassName("emergency-tasks")[0]
+
+laranja.style.backgroundColor = "rgb(255, 159, 132)";
+
+let yellow = document.getElementsByClassName("no-emergency-tasks")[0]
+
+yellow.style.backgroundColor = "rgb(249, 219, 94)";
+
+let purple = document.getElementsByTagName("h3")[0]
+
+purple.style.backgroundColor = "rgb(165, 0, 243)"
+
+let purple2 = document.getElementsByTagName("h3")[1]
+
+purple2.style.backgroundColor = "rgb(165, 0, 243)"
+
+let black = document.getElementsByTagName("h3")[2]
+
+black.style.backgroundColor = "rgb(35, 37, 37)"
+
+let black2 = document.getElementsByTagName("h3")[3]
+
+black2.style.backgroundColor = "rgb(35, 37, 37)"
+
+let baixo = document.getElementById("footer-container")
+
+baixo.style.backgroundColor = "rgb(0, 53, 51)"
