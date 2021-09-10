9f5134566875cbd4eb3b74b3576b765dd20e641c
Luan Vitor <luan_vittor@hotmail.com>
2021-09-10 16:04:02 -0300

    feito ate dft destruction

---
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/Aula/SpreadOPerator.js
@@ -0,0 +1,14 @@
+// Para fixar e praticar, vamos fazer uma salada de frutas com itens adicionais que você desejar. Faça uma função chamada fruitSalad passando como parâmetro specialFruit e additionalItens , faça a função retornar uma lista única contendo todos os itens da nossa salada de frutas usando o spread .
+
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['Maca', 'Uva', 'Banana'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['acai', 'leite', 'acucar'];
+
+const fruitSalad = (fruit, additional) => {
+  const salad = [...fruit, ...additional];
+  return salad;
+};
+
+console.log(fruitSalad(specialFruit, additionalItens));
