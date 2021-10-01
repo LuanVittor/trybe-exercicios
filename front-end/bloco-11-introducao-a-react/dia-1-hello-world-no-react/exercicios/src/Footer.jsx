01d2c2b1134c88ff5acd7bc40d1b66d87261cfde
Luan Vitor <luan_vittor@hotmail.com>
2021-10-01 18:41:14 -0300

    dia 11.1 Done

---
+++ front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/exercicios/src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className='card'>
+        <h1> E isso eh soh o comeco...</h1>
+      </div>
+    )
+  }
+}
+
+export default Footer
