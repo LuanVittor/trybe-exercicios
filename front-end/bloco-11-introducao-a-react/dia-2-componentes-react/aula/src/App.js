00cfd5dbe2c92263298ec5443ae37e086ab37df4
Luan Vitor <luan_vittor@hotmail.com>
2021-10-05 17:13:22 -0300

    Aula feita

--- front-end/bloco-11-introducao-a-react/dia-2-componentes-react/aula/src/App.js
+++ front-end/bloco-11-introducao-a-react/dia-2-componentes-react/aula/src/App.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import './App.css';
 import Order from './order';
+import UserProfile from './UserProfile';
+import Image from './Image';
+import PropTypes, { string } from 'prop-types';
 
 class App extends React.Component {
   render() {
@@ -25,11 +28,30 @@ class App extends React.Component {
       }
     };
 
+    const joao = {
+      id: 102,
+      name: "João",
+      email: "joao@gmail.com",
+      avatar: "https:\/\/cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+    };
+
+    const amelia = {
+      id: 77,
+      name: "Amélia",
+      email: "amelia@gmail.com",
+      avatar: "https:\/\/cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+    };
+
     return (
       <div className="App">
         <h1> Orders recently created </h1>
          <Order order={headphone} />
          <Order order={energyDrink} />
+         <UserProfile user={joao} />
+         <Image source={joao.avatar} alternativeText='avatar' />
+         <UserProfile user={amelia} />
+         <Image source={amelia.avatar} alternativeText='avatar' />
+
       </div>
     );
   }
