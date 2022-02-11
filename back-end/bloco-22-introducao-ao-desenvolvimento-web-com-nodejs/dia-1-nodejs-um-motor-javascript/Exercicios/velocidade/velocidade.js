871760060e45841a12c753089953502cb023a996
Luan Vitor <luan_vittor@hotmail.com>
2022-02-11 15:29:37 -0300

    day 22.1 not done

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/Exercicios/velocidade/velocidade.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/Exercicios/velocidade/velocidade.js
@@ -1,12 +1,27 @@
-const readline = require('readline-sync');
+// const readline = require('readline-sync');
 
-function calculaVelocidadeMed () {
-  const distancia = readline.questionInt('Distância percorrida (m): ');
-  const tempo = readline.questionInt('Tempo gasto (s): ');
+// function calculaVelocidadeMed () {
+//   const distancia = readline.questionInt('Distância percorrida (m): ');
+//   const tempo = readline.questionInt('Tempo gasto (s): ');
   
-  const velocidadeMedia = (distancia / tempo).toFixed(2);
+//   const velocidadeMedia = (distancia / tempo).toFixed(2);
 
-  console.log(`Velocidade média: ${velocidadeMedia} m/s`);
+//   console.log(`Velocidade média: ${velocidadeMedia} m/s`);
+// }
+
+// calculaVelocidadeMed();
+
+function subtract(...[a, b]) {
+  return a - b;
 }
 
-calculaVelocidadeMed();
+function test () {
+  setTimeout(function(){console.log(2)}, 500);
+  console.log(1);
+  setTimeout(function(){console.log(3)}, 1000);
+  setTimeout(function(){console.log(4)}, 0);
+  console.log(6);
+  setTimeout(function(){console.log(5)}, 2000);
+  }
+
+  test()
