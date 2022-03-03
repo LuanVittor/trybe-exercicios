d7ef3ab9b291df19200e98d79a0bf0bd078219b9
Luan Vitor <luan_vittor@hotmail.com>
2022-03-03 15:14:07 -0300

    bug fix: paths

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/models/reqApi.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/models/reqApi.js
@@ -2,13 +2,9 @@ const fetch = require('node-fetch');
 
 const lookupCep = async (cepToLookup) => {
   const response = await fetch(`https://viacep.com.br/ws/${cepToLookup}/json/`);
-
   if (!response.ok) return null;
-
   const address = await response.json();
-
   if (address.erro) return null;
-
   return address;
 };
 
