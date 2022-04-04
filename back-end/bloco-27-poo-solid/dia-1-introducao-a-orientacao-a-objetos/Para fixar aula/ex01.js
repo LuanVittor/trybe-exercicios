1ccc2e681cbaa5efdd7b9cd5481adb89d296ad53
Luan Vitor <luan_vittor@hotmail.com>
2022-04-04 15:55:50 -0300

    to fix done

--- back-end/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/Para fixar aula/ex01.js
+++ back-end/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/Para fixar aula/ex01.js
@@ -1,16 +1,34 @@
 var Tv = /** @class */ (function () {
     function Tv(brand, size, resolution, connections) {
         console.log("Buying TV ".concat(brand));
-        this.brand = brand;
-        this.size = size;
-        this.resolution = resolution;
-        this.connections = connections;
+        this._brand = brand;
+        this._size = size;
+        this._resolution = resolution;
+        this._connections = connections;
     }
     Tv.prototype.turnOn = function () {
-        console.log("ligando a tv da marca ".concat(this.brand));
+        console.log("ligando a tv da marca ".concat(this._brand));
     };
+    Object.defineProperty(Tv.prototype, "connectedTo", {
+        get: function () {
+            return this._connectedTo;
+        },
+        set: function (value) {
+            if (this._connections.includes(value)) {
+                this._connectedTo = value;
+                console.log(this._connectedTo);
+            }
+            else {
+                console.log('Sorry, connection unavailable!');
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Tv;
 }());
 var tv1 = new Tv('LG', 85, 2160, ['hdmi', 'vga']);
-console.log(tv1.connections);
+// console.log(tv1.connections);
 tv1.turnOn();
+tv1.connectedTo = 'hdmi';
+console.log('Connected to: ', tv1.connectedTo);
--- back-end/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/Para fixar aula/ex01.ts
+++ back-end/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/Para fixar aula/ex01.ts
@@ -1,24 +1,39 @@
 class Tv {
-  brand: string;
-  size: number;
-  resolution: number;
-  connections: string[];
-  connectedTo: string;
+  private _brand: string;
+  private _size: number;
+  private _resolution: number;
+  private _connections: string[];
+  private _connectedTo: string;
 
   constructor(brand: string, size: number, resolution: number, connections: string[]) {
     console.log(`Buying TV ${brand}`);
-    this.brand = brand;
-    this.size = size;
-    this.resolution = resolution;
-    this.connections = connections;
+    this._brand = brand;
+    this._size = size;
+    this._resolution = resolution;
+    this._connections = connections;
   }
 
-  turnOn() {
-    console.log(`ligando a tv da marca ${this.brand}`)
+  turnOn(): void {
+    console.log(`ligando a tv da marca ${this._brand}`)
+  }
+  
+  get connectedTo(): string {
+    return this._connectedTo;
+  }
+
+  set connectedTo(value: string) {
+    if (this._connections.includes(value)) {
+      this._connectedTo = value;
+      console.log(this._connectedTo);
+    } else {
+      console.log('Sorry, connection unavailable!');
+    }
   }
 }
 
 const tv1 = new Tv('LG', 85, 2160, ['hdmi', 'vga'])
-console.log(tv1.connections);
+// console.log(tv1.connections);
 tv1.turnOn()
+tv1.connectedTo = 'Wi-Fi';
+console.log('Connected to: ', tv1.connectedTo);
 
