47b330ce3b0184aa692bfa83a1b4316601e22d4d
Luan Vitor <luan_vittor@hotmail.com>
2022-04-04 15:07:55 -0300

    starting new day

---
+++ back-end/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/Para fixar aula/sintaxeGeral.js
@@ -0,0 +1,33 @@
+class Person {
+  name: string;
+  height: number;
+  weight: number;
+
+  constructor(n: string, h: number, w: number) {
+    console.log(`Creating person ${n}`);
+    this.name = n;
+    this.height = h;
+    this.weight = w;
+  }
+
+  sleep() {
+    console.log(`${this.name}: zzzzzzz`);
+  }
+}
+
+const p1 = new Person('Maria', 171, 58);
+const p2 = new Person('João', 175, 66);
+console.log(p1.name, p1.height, p1.weight);
+console.log(p2.name, p2.height, p2.weight);
+p1.sleep();
+p2.sleep();
+
+/*
+Saída:
+Creating person Maria
+Creating person João
+Maria 171 58
+João 175 66
+Maria: zzzzzzz
+João: zzzzzzz
+*/
