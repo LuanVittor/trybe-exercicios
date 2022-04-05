04ae64d3e89d5fe8361f38f1d89dc29905ca6822
Luan Vitor <luan_vittor@hotmail.com>
2022-04-05 02:51:55 -0300

    ex 02 done

---
+++ back-end/bloco-27-poo-solid/dia-2-heranca-e-interfaces/ex02.ts
@@ -0,0 +1,51 @@
+// ./Student.ts
+
+import Person from "./ex1";
+
+export default class Student extends Person {
+    private _enrollment: string = String();
+    private _examsGrades: number[] = [];
+    private _worksGrades: number[] = [];
+
+    constructor(name: string, birthDate: Date) {
+        super(name, birthDate);
+        this.enrollment = this.generateEnrollment();
+    }
+
+    get enrollment(): string {
+        return this._enrollment;
+    }
+
+    set enrollment(value: string) {
+        if (value.length < 16) throw new Error('A matrícula deve possuir no mínimo 16 caracteres.');
+
+        this._enrollment = value;
+    }
+
+    get examsGrades(): number[] {
+        return this._examsGrades;
+    }
+
+    set examsGrades(value: number[]) {
+        if (value.length > 4) throw new Error('A pessoa estudante só pode possuir 4 notas de provas.');
+
+        this._examsGrades = value;
+    }
+
+    get worksGrades(): number[] {
+        return this._worksGrades;
+    }
+
+    set worksGrades(value: number[]) {
+        if (value.length > 2) throw new Error('A pessoa estudante só pode possuir 2 notas de trabalhos.');
+
+        this._worksGrades = value;
+    }
+
+    generateEnrollment(): string {
+        const randomStr = String(Date.now() * (Math.random() + 1)).replace(/\W/g, '');
+
+        return `STU${randomStr}`;
+    }
+
+}
